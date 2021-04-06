2ae8a4032dcac82607827e6c6f35a827c9522068
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-06 23:09:18 +0200

    push exercice_02

---
+++ app.js
@@ -0,0 +1,9 @@
+function changeColorInYellow(){
+  docucument.getElementById('color').style.color = yellow;
+}
+function changeColorInGreen(){
+  docucument.getElementById('color').style.fonstyle = color : yellow;
+}
+function changeColorInBlue(){
+  docucument.getElementById('color').style.fonstyle = color : yellow;
+}
